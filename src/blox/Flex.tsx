4deec741bbd335dfd22c1ecc16adaa77848135a5
Paul Jones <paulvjones@gmail.com>
2019-06-13 23:13:57 -0600

    Typescript converstion WIP

---
+++ src/blox/Flex.tsx
@@ -0,0 +1,51 @@
+import React, { forwardRef, RefForwardingComponent } from 'react'
+import Box, { BoxProps } from '@material-ui/core/Box'
+
+const Flex: React.FC<FlexProps> =
+  forwardRef((props: FlexProps, ref?: React.Ref) => {
+    const {
+      children,
+      flex,
+      flexDirection,
+      flexFull = false,
+      ...other
+    } = props
+
+    const flexProps = getFlexProps({ flex, flexDirection, flexFull })
+
+    return (
+      <Box
+        {...flexProps}
+        {...other}
+        ref={ref}
+      >
+        {children}
+      </Box>
+    )
+  })
+
+Flex.displayName = 'Flex'
+
+export default Flex
+
+const isValid = (value: String | Number | undefined): Boolean =>
+  Boolean(value) || value === 0
+
+const getFlexProps = ({
+  flex,
+  flexDirection,
+  flexFull,
+}: Partial<FlexProps>): Partial<FlexProps> => {
+  return {
+    display: 'flex',
+    flexDirection,
+    ...(isValid(flex) ? { flex } : {}),
+    ...(isValid(flex) && flexDirection === 'column' ? { minHeight: 0 } : {}),
+    ...(flexFull ? { flexDirection: 'column', flex: 1, minHeight: 0 } : {}),
+  }
+}
+
+export interface FlexProps extends BoxProps {
+  flex: String | Number
+  flexFull: Boolean
+}
